836ac710806dc43e70dd078e9f8cac3265d616e8
stla <laurent_step@yahoo.fr>
2020-11-19 12:30:55 +0100

    Jacobian

--- src/gfconfExtremes.cpp
+++ src/gfconfExtremes.cpp
@@ -1,6 +1,6 @@
-#include <random>
 #include <Rcpp.h>
 #include <boost/math/special_functions/gamma.hpp>
+#include <random>
 #ifdef _OPENMP
 #include <omp.h>
 #endif
@@ -27,49 +27,77 @@ std::vector<T> takeFirsts(const std::vector<T>& v, size_t k) {
 
 /* shuffle {0, 1, ..., n-1} ------------------------------------------------- */
 const std::vector<size_t> shuffle_n(const size_t n,
-                                     std::default_random_engine& generator) {
+                                    std::default_random_engine& generator) {
   std::vector<size_t> elems = integers_n(n);
   std::shuffle(elems.begin(), elems.end(), generator);
   return elems;
 }
 
 /* sample k integers among {0, 1, ..., n-1} --------------------------------- */
-const std::vector<size_t> sample_int(const size_t n, const size_t k, 
-                                    std::default_random_engine& generator) {
+const std::vector<size_t> sample_int(const size_t n,
+                                     const size_t k,
+                                     std::default_random_engine& generator) {
   return takeFirsts(shuffle_n(n, generator), k);
 }
 
+const double product(const Rcpp::NumericVector v) {
+  double out = 1.0;
+  for(auto i = 0; i < v.size(); i++) {
+    out *= v(i);
+  }
+  return out;
+}
 
 /* Beta-quantiles for a vector `beta` --------------------------------------- */
-Rcpp::NumericVector BetaQuantile(
-  const double g, const double s, const double a, const double prob, const Rcpp::NumericVector& beta
-){
+Rcpp::NumericVector BetaQuantile(const double g,
+                                 const double s,
+                                 const double a,
+                                 const double prob,
+                                 const Rcpp::NumericVector& beta) {
   Rcpp::NumericVector alpha = (1.0 - beta) / prob;
   Rcpp::NumericVector Q;
-  if(g == 0.0){
+  if(g == 0.0) {
     Q = a - s * log(alpha);
-  }else{
-    Q = a + s/g * (pow(alpha, -g) - 1);
+  } else {
+    Q = a + s / g * (pow(alpha, -g) - 1);
   }
   return Q;
 }
 
 /* Calculates the Jacobian -------------------------------------------------- */
-double Jacobian(const double g, const double s, const double a, const size_t Jnumb, 
-                const Rcpp::NumericVector& X, std::default_random_engine& generator){
+double Jacobian(const double g,
+                const double s,
+                const double a,
+                const size_t Jnumb,
+                const Rcpp::NumericVector& X,
+                std::default_random_engine& generator) {
   Rcpp::NumericMatrix Xchoose3(Jnumb, 3);
   const size_t n = X.size();
-  for(size_t i = 0; i < Jnumb; i++){
+  for(size_t i = 0; i < Jnumb; i++) {
     const Rcpp::IntegerVector indices = Rcpp::wrap(sample_int(n, 3, generator));
     const Rcpp::NumericVector Xsub = X[indices];
     Xchoose3(i, Rcpp::_) = Xsub;
   }
-  Rcpp::NumericMatrix Xdiff, Jmat;
+  const Rcpp::NumericMatrix Xdiff =
+      Rcpp::cbind(Xchoose3(Rcpp::_, 1) - Xchoose3(Rcpp::_, 2),
+                  Xchoose3(Rcpp::_, 2) - Xchoose3(Rcpp::_, 0),
+                  Xchoose3(Rcpp::_, 0) - Xchoose3(Rcpp::_, 1));
   double Jmean;
   if(g == 0.0) {
-    
-  }else{
-    
+    Rcpp::NumericVector Jvec(Jnumb);
+    for(size_t i = 0; i < Jnumb; i++) {
+      Jvec(i) = product(Xdiff(i, Rcpp::_));
+    }
+    Jmean = Rcpp::mean(Rcpp::abs(Jvec));
+  } else {
+    Rcpp::NumericMatrix A = 1 + g * (Xchoose3 - a) / s;
+    Rcpp::NumericVector Jmat = (log(A) * A / g / g) * Xdiff;
+    // Jmat.attr("dim") = Rcpp::Dimension(Jnumb, 3);
+    Rcpp::NumericVector Jvec(Jnumb);
+    for(size_t i = 0; i < Jnumb; i++) {
+      Jvec(i) = Rcpp::sum(Jmat[Rcpp::Range(i * Jnumb, (i + 1) * Jnumb - 1)]);
+    }
+    Jmean = Rcpp::mean(Rcpp::abs(Jvec));
   }
   return Jmean;
 }
