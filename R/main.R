5e9204e390676a9c9d5e4a80f854b40f241d8319
stla <laurent_step@yahoo.fr>
2020-11-21 13:33:21 +0100

    'gfigpd' function

--- R/main.R
+++ R/main.R
@@ -2,6 +2,16 @@
 #' @importFrom Rcpp evalCpp
 NULL
 
+thinChain <- function(chain, skip){
+  number.iterations <- nrow(chain)
+  every.ith <- c(TRUE, rep(FALSE, skip))
+  eliminate.vector <-
+    rep(
+      every.ith,
+      ceiling((number.iterations) / length(every.ith))
+    )[1:number.iterations]
+  chain[eliminate.vector, ]
+}
 
 #' Title
 #'
@@ -26,27 +36,37 @@ NULL
 #' @param burnin number of the first MCMC iterations discarded
 #' @param thin thinning number for the MCMC chain. (e.g. if it is 1 no iteration 
 #' is skipped)
+#' @param nchains number of MCMC chains to run
+#' @param nthreads number of threads to run the chains in parallel
+#' @param seeds the seeds used for the MCMC sampler; one seed per chain, or 
+#'   \code{NULL} to use random seeds
 #'
 #' @return
 #' @export
+#' @importFrom ismev gpd.fit
+#' @importFrom doParallel registerDoParallel
+#' @importFrom parallel makeCluster stopCluster
+#' @importFrom foreach foreach `%dopar%`
 #'
-#' @examples xx
+#' @examples data("rain", package = "ismev")
+#' gf <- gfigpd(rain, beta = c(0.98, 0.99))
 gfigpd <- function(
   X, beta, i = floor(0.85 * length(X)), 
   gamma.init = NA, sigma.init = NA, sd.gamma = NA, sd.sigma = NA, 
   p1 = 0.9, p2 = 0.5, lambda1 = 2, lambda2 = 10, Jnumb = 50L, 
-  iter = 10000L, burnin = 2000L, thin = 6L) {
+  iter = 10000L, burnin = 2000L, thin = 6L,
+  nchains = nthreads, nthreads = parallel::detectCores(), seeds = NULL) {
   
-  stopifnot(thin >= 1L)
-
+  stopifnot(thin >= 1L, nchains >= 1L, nthreads >= 1L)
+  nthreads <- min(nthreads, nchains)
+  
   X <- sort(X) # -->> so there's no need to sort in C++
-  n <- length(X)
-  
+
   # Intialize the default values for the tuning parameters of the MCMC chain.
   if(is.na(gamma.init) || is.na(sigma.init)) {
     mle.fit <- gpd.fit(X, X[i], show = FALSE)
-    if (is.na(gamma.init)) gamma.init <- mle.fit$mle[2L]
-    if (is.na(sigma.init)) sigma.init <- mle.fit$mle[1L]
+    if(is.na(gamma.init)) gamma.init <- mle.fit$mle[2L]
+    if(is.na(sigma.init)) sigma.init <- mle.fit$mle[1L]
   }
   if(is.na(sd.gamma)) sd.gamma <- 2 * abs(g) / 3
   if(is.na(sd.sigma)) sd.sigma <- 2 * s / 3
@@ -54,38 +74,88 @@ gfigpd <- function(
   skip.number <- thin - 1L
   number.iterations <- (skip.number + 1L) * iter + burnin
   
-  # run the MCMC chain.
-  x.t <- gfconfExtremes:::MCMCchain(
-    X, beta, gamma.init, sigma.init, i, 
-    p1, p2, lambda1, lambda2, sd.gamma, sd.sigma,
-    number.iterations, burnin, Jnumb, 666L
-  )
-  
-  # Thin the chain by keeping every skip.number+1 iteration of the MCMC chain
-  number.iterations <- nrow(x.t)
-  every.ith <- c(TRUE, rep(FALSE, skip.number))
-  eliminate.vector <-
-    rep(
-      every.ith,
-      ceiling((number.iterations) / length(every.ith))
-    )[1:number.iterations]
-  
-  x.t <- x.t[eliminate.vector, ]
-  
-
-  # Indictor for the acceptance rate. // pas besoin avec coda::rejectionRate
-  ii <- integer(nrow(x.t))
-  for (i in 1L:(nrow(x.t) - 1L)) {
-    if (x.t[i, 4L] != x.t[i + 1L, 4L]) {
-      ii[i + 1L] <- 1L
+  if(is.null(seeds)){
+    seed1 <- sample.int(2000000L, 1)
+    seeds <- seed1 + 2000000L * (0L:(nchains-1L))
+  }else{
+    if(length(seeds) != nchains){
+      stop(
+        "Please specify one seed per chain."
+      )
     }
+    seeds <- abs(as.integer(seeds))
   }
-  acceptance.rate <- mean(ii)
-  cat("acceptance rate: ", acceptance.rate)
   
-  x.t[, 4L:(3L + length(beta))] <- x.t[, 4L:(3L + length(beta))] + X[1L]
+  params <- c("gamma", "sigma", "index", paste0("beta", seq_along(beta)))
   
+  # run the MCMC chain
+  if(nchains == 1L){
+    chain <- thinChain(MCMCchain(
+      X, beta, gamma.init, sigma.init, i, 
+      p1, p2, lambda1, lambda2, sd.gamma, sd.sigma,
+      number.iterations, burnin, Jnumb, seeds[1L]
+    ), skip.number)
+    colnames(chain) <- params
+    chain[, 4L:(3L + length(beta))] <- chain[, 4L:(3L + length(beta))] + X[1L]
+  }else{
+    if(nthreads == 1L){
+      chains <- vector("list", nchains)
+      for(k in 1L:nchains){
+        chains[[k]] <- MCMCchain(
+          X, beta, gamma.init, sigma.init, i, 
+          p1, p2, lambda1, lambda2, sd.gamma, sd.sigma,
+          number.iterations, burnin, Jnumb, seeds[k]
+        )
+      }
+    }else{
+      # nblocks <- ceiling(nchains/nthreads)
+      # blocks <- vector("list", nblocks)
+      # nthreads <- 
+      #   c(rep(nthreads, nblocks-1L), nchains - ((nblocks-1L)*nthreads))
+      # for(b in 1L:nblocks){
+      #   registerDoParallel(cores = nthreads[b])
+      # }
+      cl <- makeCluster(nthreads)
+      registerDoParallel(cl)
+      chains <- foreach(
+        k = 1L:nchains, .combine = list, .multicombine = TRUE, 
+        .export = "MCMCchain"
+      ) %dopar% MCMCchain(
+        X, beta, gamma.init, sigma.init, i, 
+        p1, p2, lambda1, lambda2, sd.gamma, sd.sigma,
+        number.iterations, burnin, Jnumb, seeds[k]
+      )
+      stopCluster(cl)
+    }
+    chains <- lapply(chains, thinChain, skip = skip.number)
+    chains <- lapply(chains, `colnames<-`, value = params)
+    chains <- lapply(chains, function(chain){
+      chain[, 4L:(3L + length(beta))] <- chain[, 4L:(3L + length(beta))] + X[1L]
+      chain
+    })
+    # threshold per chain ? non, combiner...
+  }
   
+  if(nchains == 1L){
+    out <- coda::as.mcmc(chain)
+    attr(out, "threshold") <- NA
+  }else{
+    out <- as.mcmc.list(chains)
+  }
   
-  return(x.t)
+  return(out)
+
+  # # Indictor for the acceptance rate. // pas besoin avec coda::rejectionRate
+  # ii <- integer(nrow(x.t))
+  # for (i in 1L:(nrow(x.t) - 1L)) {
+  #   if (x.t[i, 4L] != x.t[i + 1L, 4L]) {
+  #     ii[i + 1L] <- 1L
+  #   }
+  # }
+  # acceptance.rate <- mean(ii)
+  # cat("acceptance rate: ", acceptance.rate)
+  # 
+  # x.t[, 4L:(3L + length(beta))] <- x.t[, 4L:(3L + length(beta))] + X[1L]
+  # 
+  # return(x.t)
 }
